c8124a1f841d12123f6b22ae11f8e5fcaba3efac
pulipakaa24 <pulipakaaditya@gmail.com>
2026-01-21 23:23:01 -0600

    logging

--- backend/main.py
+++ backend/main.py
@@ -14,4 +14,12 @@ app.include_router(overshoot_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Add .%(msecs)03d to the end of asctime for both formatters
+    uvicorn.config.LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s.%(msecs)03d [%(name)s] %(levelprefix)s %(message)s"
+    uvicorn.config.LOGGING_CONFIG["formatters"]["access"]["fmt"] = '%(asctime)s.%(msecs)03d [%(name)s] %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
+    
+    # Use the custom date format (without milliseconds here, as they are added above)
+    uvicorn.config.LOGGING_CONFIG["formatters"]["default"]["datefmt"] = "%Y-%m-%d %H:%M:%S"
+    uvicorn.config.LOGGING_CONFIG["formatters"]["access"]["datefmt"] = "%Y-%m-%d %H:%M:%S"
+    
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="debug", log_config=uvicorn.config.LOGGING_CONFIG)
